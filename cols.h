ef8416ccf2d68d55de973ff2369bd67104458de4
ritorobbob <ritorobbob@gmail.com>
2023-06-29 21:49:39 +0100

    initial already with 3 patches

---
+++ cols.h
@@ -0,0 +1,29 @@
+static const char *colorname[] = {
+	/* 8 normal colors */
+    "#16161D", //      black
+    "#C34043", //      red
+    "#76946A", //      green
+    "#C0A36E", //      yellow
+    "#7E9CD8", //      blue
+    "#957FB8", //      magenta
+    "#6A9589", //      cyan
+	"gray90",
+
+	/* 8 bright colors */
+	"gray50",
+    "#E82424", //      brightred
+    "#98BB6C", //      brightgreen
+    "#E6C384", //      brightyellow
+    "#7E9CD8", //      brightblue
+    "#938AA9", //      brightmagenta
+    "#7AA89F", //      brightcyan
+	"white",
+
+	[255] = 0,
+
+	/* more colors can be added after 255 to use with DefaultXX */
+	"#957FB8",
+	"#555555",
+	"gray90", /* default foreground colour */
+	"#0d0c0c", /* default background colour */
+};
